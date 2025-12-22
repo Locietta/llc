30bcc23eb4ff02d43aa150f2b89f2885e91e88d7
Locietta <locietta@gmail.com>
2025-12-22 16:02:39 +0800

    chore: move a+b demo into examples

---
+++ examples/A+B/xmake.lua
@@ -0,0 +1,10 @@
+
+target("a+b")
+    set_kind("binary")
+    add_rules("slang", {
+        language_version = "2026"
+    })
+    add_files("main.cpp")
+    add_files("shaders/*.slang")
+    add_packages("fmt", "slang-rhi")
+    add_deps("util")
